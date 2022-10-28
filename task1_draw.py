d6cc5b13a46d32aaba6f3e70b8ec4e10fcc1fbd3
Biryukov239 <gregory239@mail.ru>
2022-10-28 14:01:12 +0300

    lab_pr_1 1 task is done

--- task1_draw.py
+++ task1_draw.py
@@ -13,6 +13,7 @@ screen = pygame.display.set_mode((400, 400))
 screen.fill(WHITE)
 pygame.display.flip()
 circle(screen, (255, 255, 0), (x_circle, y_circle), radius)
+
 circle(screen, (255, 0, 0), (x_circle - radius / 2, y_circle - radius / 3), radius / 5)
 circle(screen, BLACK, (x_circle - radius / 2, y_circle - radius / 3), radius / 12)
 
@@ -21,9 +22,15 @@ circle(screen, BLACK, (x_circle + radius / 2, y_circle - radius / 3), radius / 1
 
 rect(screen, BLACK, (x_circle - radius / 2, y_circle + radius * 1 / 2, 150, 30))
 
-polygon(screen, BLACK, [(x_circle - radius, y_circle - 0.8 * radius), (x_circle - radius - 5, y_circle - 0.8 * radius + 5),
-                               (x_circle - 20, y_circle - 50), (x_circle - 15, y_circle - 55)])
+polygon(screen, BLACK, [(x_circle - radius / 2 + radius / 5 + 10, y_circle - radius / 3 - radius / 12),
+                        (x_circle - radius / 2 + radius / 5 + 20, y_circle - radius / 3 - radius / 12 - 15),
+                        (x_circle - radius + 10, y_circle - 2 * radius / 3 - 35),
+                        (x_circle - radius, y_circle - 2 * radius / 3 - 20),])
 
+polygon(screen, BLACK, [(x_circle + radius / 2 - radius / 6.5 - 20, y_circle - radius / 3 - radius / 12),
+                       (x_circle + radius / 2 - radius / 6.5 - 25, y_circle - radius / 3 - radius / 12 - 15),
+                       (x_circle + radius - 15, y_circle - 2 * radius / 3 - 8),
+                       (x_circle + radius - 10, y_circle - 2 * radius / 3 + 7)])
 finished = False
 pygame.display.update()
 clock = pygame.time.Clock()
